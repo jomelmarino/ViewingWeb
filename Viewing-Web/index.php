d9a3656f8d2d71e734af17f3d0155fbdee8e88b0
jomelmarino <168659663+jomelmarino@users.noreply.github.com>
2024-05-13 20:01:20 +0800

    Add files via upload

---
+++ Viewing-Web/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LOGIN</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+	
+     <form class="form" action="login.php" method="post">
+     	<h2>LOGIN</h2>
+     	<?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
+     	<label>Student id</label>
+     	<input type="text" name="studentid" placeholder="Student ID"><br>
+
+     	<label>Email</label>
+     	<input  name="email" placeholder=" Email "><br>
+
+		 <label>Course</label>
+     	<input  name="course" placeholder=" Your course "><br>
+
+     	<button type="submit">Login</button>
+     </form>
+</body>
+</html>
+
---
+++ Viewing-Web/login.php
@@ -0,0 +1,60 @@
+<?php 
+session_start(); 
+include "db_conn.php";
+
+if (isset($_POST['studentid']) && isset($_POST['email']) && ($_POST['course'])) {
+
+	function validate($data){
+       $data = trim($data);
+	   $data = stripslashes($data);
+	   $data = htmlspecialchars($data);
+	   return $data;
+	}
+
+	$uname = validate($_POST['studentid']);
+	$pass = validate($_POST['email']);
+	$course = validate($_POST['course']);
+
+	if (empty($uname)) {
+		header("Location: index.php?error=Student ID is required");
+	    exit();
+	}
+	else if(empty($pass)){
+        header("Location: index.php?error=Email is required");
+	    exit();
+	}else if(empty($course)){
+        header("Location: index.php?error=course is required");
+	    exit();
+	}
+	
+	else{
+		$sql = "SELECT * FROM students WHERE student_id='$uname' AND email='$pass' AND course='$course' ";
+
+		$select_user = mysqli_query($conn, $sql);
+
+		if (mysqli_num_rows($select_user) === 1) {
+			$row = mysqli_fetch_assoc($select_user);
+            if ($row['student_id'] === $uname && $row['email'] === $pass) {
+            	$_SESSION['student_id'] = $row['student_id'];
+            	$_SESSION['course'] = $row['course'];
+            	$_SESSION['id'] = $row['id'];
+            	header("Location: dashboard.php");
+		        exit();
+            }
+			
+			else{
+				header("Location: index.php?error=Incorect Student ID, Email Or course");
+		        exit();
+			}
+
+		}else{
+			header("Location: index.php?error=Incorect Student ID, Email Or course ");
+	        exit();
+		}
+	}
+	
+}else{
+	header("Location: index.php");
+	exit();
+}
+?>
